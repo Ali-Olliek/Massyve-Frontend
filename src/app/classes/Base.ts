c42816cef8ac42d5136919970aeb6ad71eec8112
Ali-Olliek <Olliek95@gmail.com>
2024-11-03 15:00:28 +0200

    Adjusted Model Properties

--- src/app/classes/Base.ts
+++ src/app/classes/Base.ts
@@ -4,7 +4,7 @@ export class Base {
   public updatedAt: Date;
   constructor(base: any) {
     this.id = base.id;
-    this.createdAt = base.created_at;
-    this.updatedAt = base.updated_at;
+    this.createdAt = base.createdAt;
+    this.updatedAt = base.updatedAt;
   }
 }
--- src/app/classes/User.ts
+++ src/app/classes/User.ts
@@ -4,10 +4,12 @@ export class User extends Base {
   public email: string;
   public username: string;
   public refreshToken: string;
+  public accessToken: string;
   constructor(user: any) {
     super(user);
     this.email = user.email;
     this.username = user.username;
     this.refreshToken = user.refreshToken;
+    this.accessToken = user.accessToken;
   }
 }
