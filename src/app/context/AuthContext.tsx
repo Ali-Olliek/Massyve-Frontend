a5f1cd04beb872700594a3483396191b1b0dcab4
Ali-Olliek <Olliek95@gmail.com>
2024-11-03 14:59:38 +0200

    Created Authentication Context

--- src/app/context/AuthContext.tsx
+++ src/app/context/AuthContext.tsx
@@ -1,32 +1,74 @@
 // client/src/contexts/UserContext.tsx
-import React, { createContext, useContext, useState, ReactNode } from 'react';
+'use client';
+import React, {
+  createContext,
+  useContext,
+  useState,
+  ReactNode,
+  useEffect,
+} from 'react';
 import { User } from '../classes/User';
 
 interface AuthContextType {
   user: User | null;
   setUser: (user: User | null) => void;
-  accessToken: string | null;
-  setAccessToken: (token: string | null) => void;
 }
 
 // Initialize the UserContext with an undefined default value for typing
-const AuthContext = createContext<AuthContextType | undefined>(undefined);
+export const AuthContext = createContext<AuthContextType | undefined>(
+  undefined
+);
 
 // Define props type for the provider
 interface AuthProviderProps {
   children: ReactNode;
 }
 
-// UserProvider component that provides user and accessToken states and setters
+// External setter placeholders
+let externalSetUser: (user: User | null) => void;
+
+// AuthProvider component that provides user and accessToken states and setters
 export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
-  const [user, setUser] = useState<User | null>(null);
-  const [accessToken, setAccessToken] = useState<string | null>(null);
+  const [user, setUser] = useState<User | null>(() => {
+    const savedUser = localStorage.getItem('user');
+    return savedUser ? JSON.parse(savedUser) : null;
+  });
+
+  useEffect(() => {
+    const handleStorageChange = (event: StorageEvent) => {
+      if (event.key === 'user') {
+        setUser(event.newValue ? JSON.parse(event.newValue) : null);
+      }
+    };
+
+    window.addEventListener('storage', handleStorageChange);
+
+    // Cleanup function
+    return () => {
+      window.removeEventListener('storage', handleStorageChange);
+    };
+  }, []);
+
+  // Set the external functions to the state setters
+  externalSetUser = setUser;
 
   return (
-    <AuthContext.Provider
-      value={{ user, setUser, accessToken, setAccessToken }}
-    >
+    <AuthContext.Provider value={{ user, setUser }}>
       {children}
     </AuthContext.Provider>
   );
+};
+
+// Export the setter functions for external use
+export const setUserExternally = (user: User | null) => {
+  if (externalSetUser) externalSetUser(user);
+};
+
+// Custom hook for using AuthContext
+export const useAuthContext = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error('useAuthContext must be used within an AuthProvider');
+  }
+  return context;
 };
