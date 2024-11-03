f1a3bee731e9b7e3f65c452e847437094166c332
Ali-Olliek <Olliek95@gmail.com>
2024-11-03 10:21:18 +0200

    Added Sign Up Page

---
+++ src/app/auth/signup/page.tsx
@@ -0,0 +1,82 @@
+'use client';
+import './page.css';
+import React, { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import ROUTES from '@/app/config/routes';
+import { signUpService } from '../../services/AuthService';
+
+function SignUp() {
+  const navigate = useRouter();
+
+  const [credentials, setCredentials] = useState({
+    username: '',
+    email: '',
+    password: '',
+  });
+
+  const handleSignUp = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const user = await signUpService(credentials);
+
+    if (!user) return setCredentials({ username: '', email: '', password: '' });
+
+    navigate.push(ROUTES.main);
+  };
+
+  return (
+    <div className='sign-up-page'>
+      <div className='sign-up'>
+        <form onSubmit={handleSignUp}>
+          <div className='inputs'>
+            <label>
+              Username
+              <input
+                autoComplete='username'
+                type='text'
+                name='username'
+                placeholder='Insert an username'
+                value={credentials.username}
+                onChange={(e) =>
+                  setCredentials({ ...credentials, username: e.target.value })
+                }
+              />
+            </label>
+            <label>
+              Email
+              <input
+                type='email'
+                name='email'
+                placeholder='Insert an email'
+                value={credentials.email}
+                onChange={(e) =>
+                  setCredentials({ ...credentials, email: e.target.value })
+                }
+              />
+            </label>
+            <label>
+              Password
+              <input
+                type='password'
+                name='password'
+                placeholder='Insert a password'
+                value={credentials.password}
+                onChange={(e) =>
+                  setCredentials({ ...credentials, password: e.target.value })
+                }
+              />
+            </label>
+          </div>
+          <div>
+            <button type='submit'>Sign Up</button>
+          </div>
+        </form>
+        <p>
+          Alread have an account? Sign In
+          <a href={`/${ROUTES['sign-in']}`}>Here</a>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default SignUp;
