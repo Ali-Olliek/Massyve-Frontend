671c63fcfb6f4a73082afadc6d241aee0f02bf47
Ali-Olliek <Olliek95@gmail.com>
2024-11-03 10:24:08 +0200

    Created LocalStorageService Singleton

---
+++ src/app/services/LocalStorageService.ts
@@ -0,0 +1,41 @@
+import { User } from '../classes/User';
+
+export class AuthenticatedUser extends User {
+  public accessToken: string;
+  constructor(user: any) {
+    super(user);
+    this.accessToken = user.accessToken;
+  }
+}
+
+export default class LocalStorageService {
+  private static instance: LocalStorageService;
+
+  private constructor() {}
+
+  public static getInstance(): LocalStorageService {
+    if (!LocalStorageService.instance) {
+      LocalStorageService.instance = new LocalStorageService();
+    }
+
+    return LocalStorageService.instance;
+  }
+
+  saveUser(user: AuthenticatedUser | Partial<AuthenticatedUser>) {
+    localStorage.setItem('user', JSON.stringify(user));
+  }
+
+  deleteUser() {
+    localStorage.removeItem('user');
+  }
+
+  getUser(): AuthenticatedUser | null {
+    let userData = localStorage.getItem('user');
+
+    if (!userData) return null;
+
+    const user = new AuthenticatedUser(JSON.parse(userData));
+
+    return user;
+  }
+}
