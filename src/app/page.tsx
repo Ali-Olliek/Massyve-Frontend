1bbfdb1cafe20083692da16fabe0d4f77fcaf836
Ali-Olliek <Olliek95@gmail.com>
2024-11-03 14:59:09 +0200

    Added Main Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,40 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client';
+import React, { useContext } from 'react';
+import { AuthContext } from './context/AuthContext';
+import { AuthGuard } from './components/AuthGuard';
+import { refreshTokenService, signOutService } from './services/AuthService';
+import { useRouter } from 'next/navigation';
+import ROUTES from './config/routes';
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const userContext = useContext(AuthContext);
+  if (!userContext) {
+    throw new Error('User context is not defined');
+  }
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
+  const { user } = userContext;
+
+  const navigate = useRouter();
+
+  const handleRefreshToken = async () => {
+    await refreshTokenService();
+  };
+
+  const handleLogout = async () => {
+    await signOutService();
+
+    navigate.push(ROUTES['sign-in']);
+  };
+
+  return (
+    <AuthGuard>
+      <main>
+        <h1>Hello, {user?.username}</h1>
+        <h3>Your Access Token is: {user?.accessToken}</h3>
+        <button onClick={handleRefreshToken}>Request a new access token</button>
+        <br />
+        <button onClick={handleLogout}>Logout</button>
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    </AuthGuard>
   );
 }
