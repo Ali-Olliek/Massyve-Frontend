2f1e3503e7a3125b4e69ad75e8a311743f1e7c6b
Ali-Olliek <Olliek95@gmail.com>
2024-11-03 10:21:07 +0200

    Added Sign In Page

---
+++ src/app/auth/signin/page.tsx
@@ -0,0 +1,68 @@
+'use client';
+import './page.css';
+import React, { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { signInService } from '../../services/AuthService';
+import ROUTES from '../../config/routes';
+
+function SignIn() {
+  const navigate = useRouter();
+
+  const [credentials, setCredentials] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleSignIn = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const user = await signInService(credentials);
+
+    //# Handle Sign In Failure
+    if (!user) return navigate.push(ROUTES['sign-in']);
+
+    navigate.push(ROUTES['main']);
+  };
+
+  return (
+    <div className='sign-in-page'>
+      <div className='sign-in'>
+        <form onSubmit={handleSignIn}>
+          <div className='inputs'>
+            <label>
+              Email
+              <input
+                type='email'
+                name='email'
+                placeholder='Insert your email'
+                value={credentials.email}
+                onChange={(e) =>
+                  setCredentials({ ...credentials, email: e.target.value })
+                }
+              ></input>
+            </label>
+            <label>
+              Password
+              <input
+                type='password'
+                name='password'
+                placeholder='Insert your password'
+                value={credentials.password}
+                onChange={(e) =>
+                  setCredentials({ ...credentials, password: e.target.value })
+                }
+              ></input>
+            </label>
+          </div>
+          <div>
+            <button type='submit'>Sign In</button>
+          </div>
+        </form>
+        <p>
+          New user? Create Account <a href={`/${ROUTES['sign-up']}`}>Here</a>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default SignIn;
